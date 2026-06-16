dd707d65a38137236d2cbbf9421139c0ceb68961
hunterns98 <ngothangns98@gmail.com>
2026-06-16 14:12:51 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const db = firebase.database();
+
+
+window.joinRoom = function(){
+
+
+    let name =
+    document.getElementById("name").value;
+
+
+    let room =
+    document.getElementById("room").value;
+
+
+
+    if(!name || !room){
+
+        alert("Nhập đủ tên và mã phòng");
+
+        return;
+
+    }
+
+
+
+    db.ref(
+        "rooms/" + room + "/players/" + name
+    )
+    .set({
+
+        name:name,
+
+        score:0,
+
+        answer:""
+
+    });
+
+
+
+    alert(
+        "Đã vào phòng " + room
+    );
+
+
+}
